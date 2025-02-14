31d50779ffac20653052be239c43dae3dcc1f9eb
unknown <rk7362113@gmail.com>
2025-02-14 18:51:45 +0530

    Updated frontend to use Render backend

--- src/components/referpage.tsx
+++ src/components/referpage.tsx
@@ -33,7 +33,7 @@ const ReferEarn = () => {
     if (validateForm()) {
      
       try {
-        const response = await fetch("http://localhost:5000/referral", {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/referral`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
